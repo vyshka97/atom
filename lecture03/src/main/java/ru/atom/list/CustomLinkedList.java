40b029aed2e54c8084f8d181ce546c56126b5cae
vyshka97 <maks081197@yandex.ru>
2018-10-15 00:43:25 +0300

    done practise on lecture03

--- lecture03/src/main/java/ru/atom/list/CustomLinkedList.java
+++ lecture03/src/main/java/ru/atom/list/CustomLinkedList.java
@@ -1,61 +1,186 @@
 package ru.atom.list;
 
+import java.util.List;
 import java.util.Collection;
 import java.util.Iterator;
-import java.util.List;
 import java.util.ListIterator;
 
 
 public class CustomLinkedList<E> implements List<E> {
 
+    private int size;
+    private ListNode<E> head;
+
+    private ListNode<E> node(int index) {
+
+        if (head == null) {
+            return null;
+        }
+
+        ListNode<E> current;
+        int counter = 0;
+
+        for (current = head; current.getNext() != head && counter != index; current = current.getNext()) {
+            counter++;
+        }
+
+        return (counter == index) ? current : null;
+    }
+
+    private ListNode<E> node(Object o) {
+
+        if (head == null) {
+            return null;
+        }
+
+        ListNode<E> current;
+
+        for (current = head; current.getNext() != head && !current.getData().equals(o); current = current.getNext());
+
+        return (current.getData().equals(o)) ? current : null;
+    }
+
+    private void eraseListNode(ListNode<E> listNode) {
+        ListNode<E> next = listNode.getNext();
+        ListNode<E> prev = listNode.getPrev();
+
+        listNode.setNext(null);
+        listNode.setPrev(null);
+
+        if (listNode == head) {
+            if (next == head) {
+                head = null;
+            } else {
+                head = next;
+                next.setPrev(prev);
+                prev.setNext(next);
+            }
+        } else {
+            next.setPrev(prev);
+            prev.setNext(next);
+        }
+    }
+
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        throw new UnsupportedOperationException();
+        return head == null;
     }
 
     @Override
     public boolean contains(Object o) {
-        throw new UnsupportedOperationException();
+        return node(o) != null;
     }
 
     @Override
     public Iterator<E> iterator() {
-        throw new UnsupportedOperationException();
+        return new CustomLinkedListIterator<>(this);
     }
 
     @Override
     public boolean add(E e) {
-        throw new UnsupportedOperationException();
+        if (head == null) {
+            head = new ListNode<>(e, null, null);
+            head.setPrev(head);
+            head.setNext(head);
+            return true;
+        }
+
+        ListNode<E> last = head.getPrev();
+        ListNode<E> newListNode = new ListNode<>(e, last, head);
+        last.setNext(newListNode);
+        head.setPrev(newListNode);
+        return true;
     }
 
     @Override
     public boolean remove(Object o) {
-        throw new UnsupportedOperationException();
+        ListNode<E> foundLIstNode = node(o);
+
+        if (foundLIstNode == null) {
+            return false;
+        }
+
+        eraseListNode(foundLIstNode);
+
+        return true;
     }
 
     @Override
     public void clear() {
-        throw new UnsupportedOperationException();
+        for (ListNode<E> current = head; current.getNext() != head; current = current.getNext()) {
+            eraseListNode(current);
+        }
     }
 
     @Override
     public E get(int index) {
-        throw new UnsupportedOperationException();
+        ListNode<E> foundListNode = node(index);
+
+        return (foundListNode != null) ? foundListNode.getData() : null;
     }
 
     @Override
     public int indexOf(Object o) {
-        throw new UnsupportedOperationException();
+        if (head == null) {
+            return -1;
+        }
+
+        ListNode<E> current;
+        int counter = 0;
+
+        for (current = head; current.getNext() != head && !current.getData().equals(o); current = current.getNext()) {
+            counter++;
+        }
+
+        return (current.getData().equals(o)) ? counter : -1;
     }
 
     @Override
     public boolean addAll(Collection<? extends E> c) {
-        throw new UnsupportedOperationException();
+        for (E element : c) {
+            add(element);
+        }
+
+        return true;
+    }
+
+    public static class CustomLinkedListIterator<E> implements Iterator<E> {
+
+        private CustomLinkedList<E> customLinkedList;
+        private ListNode<E> current;
+
+        public CustomLinkedListIterator(CustomLinkedList<E> customLinkedList) {
+            this.customLinkedList = customLinkedList;
+            current = customLinkedList.head;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        @Override
+        public E next() {
+            if (current == customLinkedList.head && current.getNext() == customLinkedList.head) {
+                current = null;
+                return customLinkedList.head.getData();
+            }
+
+            if (current.getNext() == customLinkedList.head) {
+                E data = current.getData();
+                current = null;
+                return data;
+            }
+
+            E data = current.getData();
+            current = current.getNext();
+            return data;
+        }
     }
 
 
--- lecture03/src/main/java/ru/atom/list/ListNode.java
+++ lecture03/src/main/java/ru/atom/list/ListNode.java
@@ -1,7 +1,51 @@
 package ru.atom.list;
 
+import java.util.Objects;
+
 /**
  * Contains ref to next node, prev node and value
  */
 public class ListNode<E> {
+
+    private E data;
+    private ListNode<E> next;
+    private ListNode<E> prev;
+
+    public ListNode(E data, ListNode<E> prev, ListNode<E> next) {
+        this.data = data;
+        this.next = next;
+        this.prev = prev;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ListNode<?> listNode = (ListNode<?>) o;
+        return Objects.equals(data, listNode.data);
+    }
+
+    public E getData() {
+        return data;
+    }
+
+    public void setData(E data) {
+        this.data = data;
+    }
+
+    public ListNode<E> getNext() {
+        return next;
+    }
+
+    public void setNext(ListNode<E> next) {
+        this.next = next;
+    }
+
+    public ListNode<E> getPrev() {
+        return prev;
+    }
+
+    public void setPrev(ListNode<E> prev) {
+        this.prev = prev;
+    }
 }
